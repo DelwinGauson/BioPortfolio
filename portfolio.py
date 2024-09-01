07f6d1f1d149b189fcc4e6957295a04f668bcd8b
Delwix <111854126+DelwinGauson@users.noreply.github.com>
2024-09-01 20:38:09 +0800

    Small changes

--- portfolio.py
+++ portfolio.py
@@ -3,12 +3,10 @@ import streamlit as st
 def show_portfolio():
     st.header("Portfolio")
     
-    st.subheader("Project 1: Hello World")
-    st.write("This project was built using Python and Streamlit.")
-    st.code("""
-    def cool_function():
-        print("Hello, World!")
-    """, language="python")
+    st.subheader("Project 1: Cebu City Family Records API")
+    st.write("This project was built using C# and TSQL.")
+    st.write("This project delivers capabilities that allow for the efficient management of information regarding Families residing in a certain Barangay. It ensures that a Barangay can maintain track of the Details of each Family Member and the population.")
+    st.image("images/erdproj.png", caption="Cebu City Family Records ERD", use_column_width=True)
     
     st.subheader("Project 2: Ongoing Work")
     st.write("This project is currently in progress.")
